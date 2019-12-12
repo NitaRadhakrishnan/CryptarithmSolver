be4495900eb0edb8bedf32ff5a343f1de45c1d08
NitaRadhakrishnan <30308909+NitaRadhakrishnan@users.noreply.github.com>
2019-12-12 12:52:00 -0500

    Updated LEO+LEE with user_input comparison

--- CAL Final Project.py
+++ CAL Final Project.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 from clyngor import ASP, solve
 
 print("\n\n MENU:\n\n")
@@ -26,6 +20,7 @@ print("\n3. FOOD+FAD=DIETS\n")
 
 
 #Function to calculate the solution for the given problem and print the solution 
+
 def get_solver(filename):
     answers = solve(filename) # Returns the solution in the form of Frozenset objects 
     for answer in answers:
@@ -34,9 +29,31 @@ def get_solver(filename):
             for y in x: 
                 if(y!='x'): 
                     solution_list.append(y)
-        print("\nYour solution is:\n")
-        print(solution_list) # Print solution in appropriate format 
+        return(solution_list)
         
+
+        
+#Function to compare user's input with output from ASP solver 
+def compare_solution(solution_asp,user_input):
+    count = 0
+    for i in solution_asp:
+        for x in user_input: 
+            if i == x:
+                count = count +1 
+    if count == len(solution_asp):
+        print("Congratulations!Your answer is correct\n")
+        print(user_input)
+    else:
+        print("Oops!Sorry wrong asnwer. Correct answer is\n")
+        print(solution_asp)
+        
+
+#Function to convert frozenset object to list type 
+#To make it compatible with type of user's solution, for comparison 
+def convert_asp_soln_to_list(solution_asp):
+    return([list(x) for x in solution_asp])
+   
+    
         
 #User is asked to input the game of choice - input should be in string format 
 # Eg: Choose your game to play: EAT+THAT
@@ -46,16 +63,38 @@ solution_list=[]
 
 
 #Level 1 puzzles 
-if (choice=="DAN+NAN"):
+if (choice == "DAN+NAN"):
+    #Getting user's solution
+     
     #Function call to solve the puzzle DAN+NAN=NORA
-    get_solver("DAN+NAN=NORA.lp")
+    solution_list=get_solver("DAN+NAN=NORA.lp")
+    
+    
 if(choice == "DI+IS"):
+    #Getting user's solution
+        
     #Function call to solve the puzzle DI+IS=ILL
     get_solver("DI+IS=ILL.lp")
-if(choice == "LEO+LEE"):
-    #Function call to solve the puzzle LEO+LEE=ALL
-    get_solver("LEO+LEE=ALL.lp")
     
+if(choice == "LEO+LEE"):
+    
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = ['o','l','a','e']
+    user_input=[]
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("LEO+LEE=ALL.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
+   
+  
 #Level 2 puzzles 
 if (choice=="EAT+THAT"):
     #Function call to solve the puzzle EAT+THAT=APPLE
@@ -81,16 +120,3 @@ if(choice == "FOOD+FAD"):
     
 
                     
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
