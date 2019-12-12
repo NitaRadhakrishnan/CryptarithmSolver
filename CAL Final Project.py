158ea2b35723c1996b8ad6f5febbd340820bb2bb
NitaRadhakrishnan <30308909+NitaRadhakrishnan@users.noreply.github.com>
2019-12-12 14:35:23 -0500

    Completed code

--- CAL Final Project.py
+++ CAL Final Project.py
@@ -64,20 +64,43 @@ solution_list=[]
 
 #Level 1 puzzles 
 if (choice == "DAN+NAN"):
-    #Getting user's solution
-     
-    #Function call to solve the puzzle DAN+NAN=NORA
-    solution_list=get_solver("DAN+NAN=NORA.lp")
+    print("DAN+NAN=NORA\n")
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = ['o','d','r','a','n']
+    user_input=[]
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("DAN+NAN=NORA.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
     
     
 if(choice == "DI+IS"):
-    #Getting user's solution
-        
-    #Function call to solve the puzzle DI+IS=ILL
-    get_solver("DI+IS=ILL.lp")
+    print("DI+IS=ILL\n")
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = ['i','s','l','d']
+    user_input=[]
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("DI+IS=ILL.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
+    
     
 if(choice == "LEO+LEE"):
-    
+    print("LEO+LEE=ALL \n")
     print("Complete the solution by providing the numbers for the following letters")
     letters = ['o','l','a','e']
     user_input=[]
@@ -88,7 +111,6 @@ if(choice == "LEO+LEE"):
         item_list.append(i)
         item_list.append(int(value))
         user_input.append(item_list)
-    
     #Comparing user's solution and ASP's solution
     solution_asp = get_solver("LEO+LEE=ALL.lp")
     solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
@@ -97,26 +119,108 @@ if(choice == "LEO+LEE"):
   
 #Level 2 puzzles 
 if (choice=="EAT+THAT"):
-    #Function call to solve the puzzle EAT+THAT=APPLE
-    get_solver("EAT+THAT=APPLE.lp")
+    print("EAT+THAT=APPLE\n")
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = ['p','a','t','h','l','e']
+    user_input=[]
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("EAT+THAT=APPLE.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
+    
 if(choice == "SEEM+MEAN"):
-    #Function call to solve the puzzle SEEM+MEAN=TEAMS
-    get_solver("SEEM+MEAN=TEAMS.lp")
+    print("SEEM+MEAN=TEAMS\n")
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = ['t','s','a','e','m','n']
+    user_input=[]
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("SEEM+MEAN=TEAMS.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
+    
 if(choice == "TESS+SEES"):
-    #Function call to solve the puzzle TESS+SEES=ELLEN
-    get_solver("TESS+SEES=ELLEN.lp")
+    print("TESS+SEES=ELLEN\n")
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = ['t','e','l','s','n']
+    user_input=[]
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("TESS+SEES=ELLEN.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
+    
     
 #Level 3 puzzles 
 if (choice=="AT+EAST+WEST"):
-    #Function call to solve the puzzle AT+EAST+WEST=SOUTH
-    get_solver("AT+EAST+WEST=SOUTH.lp")
+    print("AT+EAST+WEST=SOUTH\n")
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = ['a','s','e','o','h','t','w','u']
+    user_input=[]
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("AT+EAST+WEST=SOUTH.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
+    
 if(choice == "CRACK+HACK"):
-    #Function call to solve the puzzle CRACK+HACK=ERROR
-    get_solver("CRACK+HACK=ERROR.lp")
+    print("CRACK+HACK=ERROR\n")
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = ['e','o','a','c','h','r','k']
+    user_input=[]
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("CRACK+HACK=ERROR.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
+    
 if(choice == "FOOD+FAD"):
-    #Function call to solve the puzzle TESS+SEES=ELLEN
-    get_solver("FOOD+FAD=DIETS.lp")
-
+    print("FOOD+FAD=DIETS\n")
+    print("Complete the solution by providing the numbers for the following letters")
+    letters = []
+    user_input=['i','a','t','o','s','e','f','d']
+    #Getting user's solution for each letter and appending them to the user_input list 
+    for i in letters: 
+        value=input(i)
+        item_list =[]
+        item_list.append(i)
+        item_list.append(int(value))
+        user_input.append(item_list)
+    #Comparing user's solution and ASP's solution
+    solution_asp = get_solver("FOOD+FAD=DIETS.lp")
+    solution_asp = convert_asp_soln_to_list(solution_asp)  #converting frozenset object to list 
+    compare_solution(solution_asp,user_input)
     
 
                     
